87d89146fd700849da6c410d7562b5ebc4cf6d6d
Izadora <izadoramariano323@gmail.com>
2024-10-01 21:29:02 -0300

    Estudando condição

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log('A velocidade do seu carro é ' + vel + ' Km/h')
+if(vel > 60){
+    console.log('Vecê ultrapassou a velocidade permitida, MULTADO!')//condição simples
+}
+console.log('Dirija sempre usando sinto de segurança')
